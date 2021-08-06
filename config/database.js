9205359534ff840fd1722740e159ef592cf181f1
stevtymz <stevtymez@gmail.com>
2021-08-06 02:21:40 -0700

    make it better

--- config/database.js
+++ config/database.js
@@ -1,43 +1,17 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-  
-  if (env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: 'default',
+module.exports = ({ env }) => {(
+  {
+    defaultConnection: 'default',
       connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password:config.password,
-          },
-          options: {
-            ssl: false,
-          },
-        },
-      },
-    }
-  }
-  
-  
-  return {
-  defaultConnection: 'default',
-  connections: {
     default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        connector: 'bookshelf',
+          settings: {
+          client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {
-        useNullAsDefault: true,
+        options: {
+          useNullAsDefault: true,
+      },
       },
     },
-  },
-}};
+  }
+)};
